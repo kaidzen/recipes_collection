6bf0b8d2c5eb791a8b093976626a4b213e90b683
Kaidzen <karpov.srg@gmail.com>
2015-12-27 18:51:03 +0200

    Implemented DataSource in dbInfo.java.

--- src/main/java/org/hardcrystal/food/dbHelpers/dbInfo.java
+++ src/main/java/org/hardcrystal/food/dbHelpers/dbInfo.java
@@ -1,5 +1,8 @@
 package org.hardcrystal.food.dbHelpers;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
@@ -12,36 +15,51 @@ public class dbInfo {
 
     private static Connection conn = null;
 
+
     public static Connection getConnection() {
 
-        if (conn != null) return conn;
-
-        InputStream inputStream = dbInfo.class.getClassLoader().getResourceAsStream("/db.properties");
-        Properties properties = new Properties();
-
         try {
-            properties.load(inputStream);
-            String driver = properties.getProperty("driver");
-            String hosturl = properties.getProperty("hosturl");
-            String user = properties.getProperty("user");
-            String password = properties.getProperty("password");
-
-            Class.forName(driver).newInstance();
-            conn = DriverManager.getConnection(hosturl, user, password);
-
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+            InitialContext ic = new InitialContext();
+            Context initialContext = (Context) ic.lookup("java:comp/env");
+            DataSource datasource = (DataSource) initialContext.lookup("jdbc/MySQLDS");
+            conn = datasource.getConnection();
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return conn;
     }
+
+
+//    public static Connection getConnection() {
+//
+//        if (conn != null) return conn;
+//
+//        InputStream inputStream = dbInfo.class.getClassLoader().getResourceAsStream("/db.properties");
+//        Properties properties = new Properties();
+//
+//        try {
+//            properties.load(inputStream);
+//            String driver = properties.getProperty("driver");
+//            String hosturl = properties.getProperty("hosturl");
+//            String user = properties.getProperty("user");
+//            String password = properties.getProperty("password");
+//
+//            Class.forName(driver).newInstance();
+//            conn = DriverManager.getConnection(hosturl, user, password);
+//
+//        } catch (InstantiationException e) {
+//            e.printStackTrace();
+//        } catch (IllegalAccessException e) {
+//            e.printStackTrace();
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return conn;
+//    }
 
     public static void closeConnection(Connection toBeClosed) {
         if (toBeClosed == null)
